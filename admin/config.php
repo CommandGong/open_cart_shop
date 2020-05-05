21740d471dedd7d1309f601745929bdd7a4a2953
Yanbin <yanbingong1990@gmail.com>
2020-05-05 13:34:18 -0400

    add open_cart platform to aceway web

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/aceway_cart/admin/');
+define('HTTP_CATALOG', 'http://localhost/aceway_cart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/aceway_cart/admin/');
+define('HTTPS_CATALOG', 'http://localhost/aceway_cart/');
+
+// DIR
+define('DIR_APPLICATION', 'G:/XAMPP7/htdocs/aceway_cart/admin/');
+define('DIR_SYSTEM', 'G:/XAMPP7/htdocs/aceway_cart/system/');
+define('DIR_IMAGE', 'G:/XAMPP7/htdocs/aceway_cart/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_CATALOG', 'G:/XAMPP7/htdocs/aceway_cart/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'aceway_admin');
+define('DB_PASSWORD', 'admin123');
+define('DB_DATABASE', 'aceway_cart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'aceway_cart_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
